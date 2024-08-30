224194cf7526bbd6e703f75259bddb733320bd0d
“your <kushniruk.yana@gmail.com>
2024-08-29 22:17:35 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
+import MyNutritionComponent from './MyNutritionComponent';
+
 
 function App() {
+
+const [mySearch, setMySearch] = useState("");
+const [myNutrition, setMyNutrition] = useState([]);
+
+
+
+useEffect(() => {
+  const getAnalys = async () => {
+    const response = await fetch(`https://api.edamam.com/api/nutrition-data?app_id=ee774096&app_key=%203060c83b97b24940cfe80d7b67653174%09&nutrition-type=cooking&ingr=1egg`);
+    const data = await response.json();
+    setMyNutrition(data);
+   console.log(data);
+  }
+  getAnalys()
+},[])
+
+const myNutritionAnalis = (e) => {
+ setMySearch(e.target.value)
+}
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ <div className="container">
+
+<h1>Nutrition Analysis</h1>
+</div>
+
+<div className='container'>
+  <form>
+    <input className='search' onChange={myNutritionAnalis} value={mySearch}/>
+  </form>
+</div>
+
+
+<div className='container'>
+<button className='btn'>
+<img width="80" height="64" src="https://img.icons8.com/arcade/64/citrus-1.png" alt="citrus-1"/>
+</button>
+</div>
+
+{myNutrition.map (element => (
+  <MyNutritionComponent label={element.ingredients.text} />
+))}
+
+
+  </div>
   );
 }
 
---
+++ src/MyNutritionComponent.js
@@ -0,0 +1,10 @@
+function MyNutritionComponent({label}){
+
+    return(<div>
+        <h2>{label}</h2>
+        
+    </div>)
+}
+   
+
+export default MyNutritionComponent;
