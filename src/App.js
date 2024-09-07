e73a153845382f0ea03204d13bc2fe2d6743efc4
“your <kushniruk.yana@gmail.com>
2024-09-07 19:05:59 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,58 +1,121 @@
 
+
 import { useEffect, useState } from 'react';
 import './App.css';
 import MyNutritionComponent from './MyNutritionComponent';
+import Swal from 'sweetalert2';
+import LoaderPage from './Loader/LoaderPage';
 
 
 function App() {
+  const [mySearch, setMySearch] = useState("");
+  const [myNutrition, setMyNutrition] = useState([]);
+  const[ stateLoader, setStateLoader] = useState(true);
+  
 
-const [mySearch, setMySearch] = useState("");
-const [myNutrition, setMyNutrition] = useState([]);
+  const getAnalysis = async () => {
+    try {
+      setStateLoader(true); 
+      const response = await fetch(
+        `https://api.edamam.com/api/nutrition-data?app_id=ee774096&app_key=3060c83b97b24940cfe80d7b67653174&nutrition-type=cooking&ingr=${mySearch}`,
+        {
+          method: 'GET', 
+          headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+          }
+        }
+      );
+  
+      if (response.ok) {
+        const data = await response.json();
+        setMyNutrition([data]);
+        setStateLoader(false); 
+        console.log( data);
+      } else {
+        console.error("Error fetching data:", response.status);
+      
+      }
+    } catch (error) {
+      console.error("Fetch error:", error);
+      setStateLoader(false); 
+      
+    }
+  };
 
 
+  const handleFormSubmit = (e) => {
+    e.preventDefault(); 
+    if (mySearch.trim() !== "") {
+      getAnalysis (); 
+    }
+   
+  
+  };
 
-useEffect(() => {
-  const getAnalys = async () => {
-    const response = await fetch(`https://api.edamam.com/api/nutrition-data?app_id=ee774096&app_key=%203060c83b97b24940cfe80d7b67653174%09&nutrition-type=cooking&ingr=1egg`);
-    const data = await response.json();
-    setMyNutrition(data);
-   console.log(data);
+  const myNutritionAnalis = (e) => {
+    setMySearch(e.target.value);
+  
+  };
+
+  const handleAlert = () => {
+    Swal.fire(
+     "Please, indicate the quantity of products",
+      
+    )
   }
-  getAnalys()
-},[])
+    
 
-const myNutritionAnalis = (e) => {
- setMySearch(e.target.value)
-}
+ 
+
+  useEffect(() => {
+    const timer = setTimeout(() => setStateLoader(false), 5000);
+    return () => clearTimeout(timer)
+  }, [])
+
 
 
   return (
     <div className="App">
- <div className="container">
+      <div className="container">
+        <h1>Nutrition Analysis</h1>
+      </div>
 
-<h1>Nutrition Analysis</h1>
-</div>
+      <div className="container">
+        <form onSubmit={handleFormSubmit}>
+          <input className="search" onChange={myNutritionAnalis} value={mySearch} />
+        </form>
+      </div>
 
-<div className='container'>
-  <form>
-    <input className='search' onChange={myNutritionAnalis} value={mySearch}/>
-  </form>
-</div>
+      <div className="container">
+        <button className="btn" onClick={handleFormSubmit}>
+          <img width="80" height="64" src="https://img.icons8.com/arcade/64/citrus-1.png" alt="citrus-1" />
+        </button>
+      </div>
 
+      {stateLoader && <LoaderPage />} 
 
-<div className='container'>
-<button className='btn'>
-<img width="80" height="64" src="https://img.icons8.com/arcade/64/citrus-1.png" alt="citrus-1"/>
-</button>
-</div>
-
-{myNutrition.map (element => (
-  <MyNutritionComponent label={element.ingredients.text} />
-))}
-
-
-  </div>
+      {myNutrition.length > 0 && myNutrition.map((element, index) => (
+        <MyNutritionComponent key={index} label={/*element.ingredients ? */element.ingredients[0].text && handleAlert()}
+        calories={element.calories} 
+        waight={element.totalWeight}
+        calcium={element.totalNutrients.CA.quantity}
+        carbohydrate={element.totalNutrients.CHOCDF.quantity} 
+        cholesterol = {element.totalNutrients.CHOLE.quantity}
+        energy = {element.totalNutrients.ENERC_KCAL.quantity}
+        totalLipid = {element.totalNutrients.FAT.quantity}
+        sugar = {element.totalNutrients.SUGAR.quantity} />
+      ))}
+    </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
--- src/MyNutritionComponent.js
+++ src/MyNutritionComponent.js
@@ -1,7 +1,21 @@
-function MyNutritionComponent({label}){
+function MyNutritionComponent({ingredients, calories, waight, calcium, carbohydrate, cholesterol, energy, totalLipid, sugar }){
+    
 
     return(<div>
-        <h2>{label}</h2>
+
+
+    <h2> {ingredients}</h2>
+    <h3>Amount Per Serving</h3>
+     <p> Calories:{calories.toFixed()}</p>
+    <p> Waight: {waight.toFixed()} g</p>
+    <p> Calcium: { calcium.toFixed()} mg</p>
+    <p>Carbohydrate: {carbohydrate.toFixed()} g</p>
+    <p>Cholesterol: {cholesterol.toFixed()} mg</p>
+    <p>Energy: {energy.toFixed()} kcal</p>
+    <p>Total lipid: {totalLipid.toFixed() } g</p>
+    <p> Sugar: {sugar.toFixed()} g</p>
+
+    
         
     </div>)
 }
